c033944b5eb037345b0477dd094ab4308664a3c2
jinwon93 <kjw8208@nate.com>
2022-11-02 15:34:48 +0900

    react-truffle

---
+++ src/config.js
@@ -0,0 +1,80 @@
+// export const CONTACT_ADDRESS = '0xfAd567EBdCb36f49F3a509FEDF9e72E3ad75ca59';
+
+
+
+export const CONTACT_ADDRESS = "0xfAd567EBdCb36f49F3a509FEDF9e72E3ad75ca59";
+
+// ABI(Application Binary Interface)란, 컨트랙트 함수와 매개변수들을 JSON 형식으로 나타낸 리스트다.
+
+export const CONTACT_ABI = [
+
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "count",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function",
+        "signature": "0x06661abd"
+      },
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "name": "contacts",
+        "outputs": [
+          {
+            "name": "id",
+            "type": "uint256"
+          },
+          {
+            "name": "name",
+            "type": "string"
+          },
+          {
+            "name": "phone",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function",
+        "signature": "0xe0f478cb"
+      },
+      {
+        "inputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor",
+        "signature": "constructor"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_name",
+            "type": "string"
+          },
+          {
+            "name": "_phone",
+            "type": "string"
+          }
+        ],
+        "name": "createContact",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function",
+        "signature": "0x3dce4920"
+      }
+];
